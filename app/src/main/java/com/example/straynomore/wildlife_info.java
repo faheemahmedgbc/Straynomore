012a2b81e7bdfa704251321255a5eaefc7eef076
101056255 <edwardphilip30@gmail.com>
2021-02-05 16:45:36 -0500

    Initial upload

---
+++ app/src/main/java/com/example/straynomore/wildlife_info.java
@@ -0,0 +1,14 @@
+package com.example.straynomore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class wildlife_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wildlife_info);
+    }
+}
