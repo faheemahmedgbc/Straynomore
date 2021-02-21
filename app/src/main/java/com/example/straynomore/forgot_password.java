677c6df4c03022e1e289ae73c6da744598a3717c
101056255 <edwardphilip30@gmail.com>
2021-02-21 16:58:53 -0500

    Forgot password page

--- app/src/main/java/com/example/straynomore/forgot_password.java
+++ app/src/main/java/com/example/straynomore/forgot_password.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
 import android.widget.ImageView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class forgot_password extends AppCompatActivity {
 
@@ -12,6 +16,23 @@ public class forgot_password extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        Button resetPassword = findViewById(R.id.btn_email_password);
+
+        resetPassword.setOnClickListener(v -> {
+            Snackbar.make(findViewById(R.id.forgot_password), "Password reset email sent! " +
+                            "\nRedirecting to log in page",
+                    Snackbar.LENGTH_LONG)
+                    .show();
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    finish();
+                }
+            }, 3000);
+        });
 
         ImageView back = findViewById(R.id.img_back);
 
